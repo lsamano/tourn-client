dcf6354b447db547f8709d1062904c64a7802157
lsamano <leizl.samano@gmail.com>
2019-03-19 16:53:40 -0400

    wip

---
+++ src/Components/TeamDropDown.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Dropdown } from 'semantic-ui-react'
+import {entryPostFetch} from '../Redux/actions';
+import {connect} from 'react-redux';
+
+class TeamDropDown extends React.Component {
+  state = {
+    team_id: ""
+  }
+
+  formattedTeams = () => {
+    return this.props.teams.map(team => {
+      return {key:team.id, text:team.name, value:team.id}
+    })
+  }
+
+  handleChange  = (event, {value}) => {
+    console.log(value);
+    this.setState({
+      team_id: value
+    })
+}
+
+  handleSubmit = event => {
+    event.preventDefault();
+    if (!this.props.enteredTeams.filter(team => team.id === this.state.team_id).length > 0) {
+      const entryInfo = {...this.state, tournament_id: this.props.tournament.id}
+      this.props.entryPostFetch(entryInfo);
+    }
+    // else render a warning message
+  }
+
+  render() {
+    return (
+      <div>
+      <Dropdown
+      placeholder='Select a Team'
+      fluid
+      selection
+      options={this.formattedTeams()}
+      name="team"
+      onChange={(event, data) => this.handleChange(event, data)}
+      />
+      <button className="ui button primary" onClick={this.handleSubmit}>Join This Tournament</button>
+      </div>
+    )
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  entryPostFetch: (entryInfo) => dispatch(entryPostFetch(entryInfo))
+})
+
+export default connect(null, mapDispatchToProps)(TeamDropDown);
