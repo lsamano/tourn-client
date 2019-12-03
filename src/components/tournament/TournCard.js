67532e4a51d0385b4bd62e500877d612a350b786
Leizl Samano <leizl.samano@gmail.com>
2019-12-03 17:22:09 -0500

    Added time to TournCards of TournIndex

--- src/components/tournament/TournCard.js
+++ src/components/tournament/TournCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Image, List } from 'semantic-ui-react';
+import moment from 'moment';
 
 const TournCard = ({ tournament }) => {
   return (
@@ -8,9 +9,10 @@ const TournCard = ({ tournament }) => {
       <Image avatar src={tournament.image} size="small"/>
       <List.Content>
         <List.Header>{tournament.title}</List.Header>
+        <div>{moment(tournament.start_dt).format('llll')}</div>
         <List.Description>
           <p>Format: Single Elimination</p>
-          {tournament.description}
+            {tournament.description}
           <p>
             <Link to={`/tournaments/${tournament.id}`}>See Tournament Info</Link>
           </p>
